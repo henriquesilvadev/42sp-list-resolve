d1d7ddbd85a81efe320eccae3b6b3ca292a06135
HENRIQUE SILVA <40860601+henriquehsilva@users.noreply.github.com>
2026-01-24 13:21:13 -0300

    Replace SIZE and VIEWS macros with constants

--- rush-01/main.c
+++ rush-01/main.c
@@ -1,16 +1,13 @@
-#define SIZE 4
-#define VIEWS 16
-
 int	parse_input(char *str, int *views);
-void	init_grid(int grid[SIZE][SIZE]);
-int	solve(int grid[SIZE][SIZE], int *views, int pos);
-void	print_grid(int grid[SIZE][SIZE]);
+void	init_grid(int grid[4][4]);
+int	solve(int grid[4][4], int *views, int pos);
+void	print_grid(int grid[4][4]);
 void	ft_putstr(char *str);
 
 int	main(int argc, char **argv)
 {
-	int	grid[SIZE][SIZE];
-	int	views[VIEWS];
+	int	grid[4][4];
+	int	views[16];
 
 	if (argc != 2 || !parse_input(argv[1], views))
 	{
