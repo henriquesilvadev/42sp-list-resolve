dd837d76bcc16cb6038758ffe7565cd271146f34
Henrique Silva <contato@henriquesilva.dev>
2026-05-15 16:42:35 -0300

    feat(Libft): create ft_strchr.c

---
+++ Libft/ft_strchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhonorio <hhonorio@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/15 14:07:15 by hhonorio          #+#    #+#             */
+/*   Updated: 2026/05/15 15:28:24 by hhonorio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	int				i;
+	unsigned char	uc;
+
+	uc = (unsigned char)c;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == uc)
+			return ((char *)&s[i]);
+		if (s[i] == '\0')
+			;
+		i++;
+	}
+	return (NULL);
+}
