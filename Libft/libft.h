e6b342d1a4a0518a898c220546011bab9c316948
Henrique Silva <contato@henriquesilva.dev>
2026-05-15 16:43:52 -0300

    feat(Libft): create libft.h

---
+++ Libft/libft.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhonorio <hhonorio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/14 15:28:40 by hhonorio          #+#    #+#             */
+/*   Updated: 2026/05/15 07:51:01 by hhonorio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+/* *************************************** */
+/* Mandatory Functions                     */
+/* Character checking functions            */
+/* *************************************** */
+
+int	ft_isupper(int c);
+int	ft_islower(int c);
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+
+#endif
