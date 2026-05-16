0802d4f50e9e399aee28c15567d8b544b5b7ca64
Henrique Silva <contato@henriquesilva.dev>
2026-05-16 08:01:39 -0300

    fix(Libft) update ft_strchr.c

--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hhonorio <hhonorio@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: hhonorio <hhonorio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/15 14:07:15 by hhonorio          #+#    #+#             */
-/*   Updated: 2026/05/15 15:28:24 by hhonorio         ###   ########.fr       */
+/*   Updated: 2026/05/16 07:56:29 by hhonorio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@ char	*ft_strchr(const char *s, int c)
 	int				i;
 	unsigned char	uc;
 
-	uc = (unsigned char)c;
 	i = 0;
-	while (s[i])
+	uc = (unsigned char)c;
+	while (s[i] != '\0')
 	{
 		if (s[i] == uc)
 			return ((char *)&s[i]);
-		if (s[i] == '\0')
-			;
 		i++;
 	}
+	if (s[i] == uc)
+		return ((char *)&s[i]);
 	return (NULL);
 }
