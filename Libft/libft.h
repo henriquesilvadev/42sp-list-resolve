a302cd01ceed18205607fc52f49e2ac9b2039835
Henrique Silva <contato@henriquesilva.dev>
2026-05-16 14:49:24 -0300

    feat(Libft) update libft.h

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,24 +6,39 @@
 /*   By: hhonorio <hhonorio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/14 15:28:40 by hhonorio          #+#    #+#             */
-/*   Updated: 2026/05/15 07:51:01 by hhonorio         ###   ########.fr       */
+/*   Updated: 2026/05/16 12:39:09 by hhonorio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <stddef.h>
+
 /* *************************************** */
 /* Mandatory Functions                     */
-/* Character checking functions            */
 /* *************************************** */
 
-int	ft_isupper(int c);
-int	ft_islower(int c);
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+int		ft_isupper(int c);
+int		ft_islower(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+
+/* *************************************** */
+/* String Manipulation                     */
+/* *************************************** */
+
+size_t	ft_strlen(char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	ft_strnstr(const char *big, const char *little, size_t len);
+
+/* *************************************** */
+/* Memory Manipulation                     */
+/* *************************************** */
 
 #endif
