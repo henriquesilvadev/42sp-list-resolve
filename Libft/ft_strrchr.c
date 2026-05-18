38fd14f5810ae1963d15845e08528fff5e7fb446
Henrique Silva <contato@henriquesilva.dev>
2026-05-18 13:07:51 -0300

    feat(Libft) update ft_strrchr.c

--- Libft/ft_strrchr.c
+++ Libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: hhonorio <hhonorio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/15 15:32:30 by hhonorio          #+#    #+#             */
-/*   Updated: 2026/05/18 10:40:03 by hhonorio         ###   ########.fr       */
+/*   Updated: 2026/05/18 13:07:01 by hhonorio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,18 +30,4 @@ char	*ft_strrchr(const char *s, int c)
 	if (s[i] == uc)
 		res = (char *) &s[i];
 	return (res);
-}
-
-int	main(void)
-{
-	char	*result;
-	char	*str;
-
-	str = "MESTRE";
-	result = ft_strrchr(str, 'E');
-	if (result)
-		printf("Encontrado: %s\n", result);
-	else
-		printf("Não encontrado\n");
-	return (0);
 }
