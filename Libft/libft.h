22828da22b623048f239a023c1e9bfbc0b05a5a4
Henrique Silva <contato@henriquesilva.dev>
2026-05-18 11:48:09 -0300

    feat(Libft) update libft.h

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,14 +6,19 @@
 /*   By: hhonorio <hhonorio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/14 15:28:40 by hhonorio          #+#    #+#             */
-/*   Updated: 2026/05/16 12:39:09 by hhonorio         ###   ########.fr       */
+/*   Updated: 2026/05/18 10:06:12 by hhonorio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+/* *************************************** */
+/* External Libraries                      */
+/* *************************************** */
+
 # include <stddef.h>
+# include <stdio.h>
 
 /* *************************************** */
 /* Mandatory Functions                     */
@@ -35,10 +40,15 @@ size_t	ft_strlen(char *str);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 /* *************************************** */
 /* Memory Manipulation                     */
 /* *************************************** */
+
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t len);
 
 #endif
