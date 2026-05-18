16f7c8eced93d22c8ed9e4aeb25e74aefd26ea8b
Henrique Silva <contato@henriquesilva.dev>
2026-05-18 11:45:56 -0300

    feat(Libft) update ft_strncmp.c

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -6,22 +6,33 @@
 /*   By: hhonorio <hhonorio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/16 08:07:03 by hhonorio          #+#    #+#             */
-/*   Updated: 2026/05/16 11:10:57 by hhonorio         ###   ########.fr       */
+/*   Updated: 2026/05/18 11:04:58 by hhonorio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
-	if (n == 0)
-		return (0);
 	i = 0;
-	while (i < n && s1[i] && s1[i] == s2[i])
+	while ((s1[i] && s2[i]) && i < n)
+	{
+		if ((unsigned char) s1[i] != (unsigned char) s2[i])
+			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
 		i++;
-	if (i == n)
-		return (0);
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	printf("%d\n", ft_strncmp("MESTRE", "MESTRE", 6));
+	printf("%d\n", ft_strncmp("MESTRE", "MESTRO", 6));
+	printf("%d\n", ft_strncmp("MESTRO", "MESTRE", 6));
+	printf("%d\n", ft_strncmp("MESTRE", "MESTRE", 3));
+	printf("%d\n", ft_strncmp("ABC", "ABD", 2));
+	printf("%d\n", ft_strncmp("", "", 5));
+	printf("%d\n", ft_strncmp("ABC", "", 3));
 }
