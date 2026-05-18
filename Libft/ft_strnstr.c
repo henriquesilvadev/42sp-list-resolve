eed3af56a14e20f41e97e689c0453ba085df477f
Henrique Silva <contato@henriquesilva.dev>
2026-05-18 13:11:19 -0300

    feat(Libft) update ft_strnstr.c

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: hhonorio <hhonorio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/16 09:04:31 by hhonorio          #+#    #+#             */
-/*   Updated: 2026/05/18 11:35:50 by hhonorio         ###   ########.fr       */
+/*   Updated: 2026/05/18 13:10:49 by hhonorio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,14 +33,4 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		j = 0;
 	}
 	return (NULL);
-}
-
-int	main(void)
-{
-	printf("%s\n", ft_strnstr("MESTRE", "EST", 6));
-	printf("%s\n", ft_strnstr("MESTRE", "EST", 2));
-	printf("%s\n", ft_strnstr("MESTRE", "XYZ", 6));
-	printf("%s\n", ft_strnstr("MESTRE", "MESTRE", 6));
-	printf("%s\n", ft_strnstr("", "EST", 6));
-	printf("%s\n", ft_strnstr("MESTRE", "RE", 6));
 }
