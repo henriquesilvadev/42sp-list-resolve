a06ce7e877e497c648a8400f82187911213da7bc
Henrique Silva <contato@henriquesilva.dev>
2026-05-18 11:43:16 -0300

    feat(Libft) update ft_strchr.c

--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -6,26 +6,40 @@
 /*   By: hhonorio <hhonorio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/15 14:07:15 by hhonorio          #+#    #+#             */
-/*   Updated: 2026/05/16 07:56:29 by hhonorio         ###   ########.fr       */
+/*   Updated: 2026/05/18 10:03:31 by hhonorio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h" 
 
 char	*ft_strchr(const char *s, int c)
 {
-	int				i;
+	unsigned int	i;
 	unsigned char	uc;
 
+	uc = (char) c;
 	i = 0;
-	uc = (unsigned char)c;
-	while (s[i] != '\0')
+	while (s[i])
 	{
 		if (s[i] == uc)
-			return ((char *)&s[i]);
+			return ((char *) &s[i]);
 		i++;
 	}
 	if (s[i] == uc)
-		return ((char *)&s[i]);
+		return ((char *) &s[i]);
 	return (NULL);
+}
+
+int	main(void)
+{
+	char	*result;
+	char	*str;
+
+	str = "MESTRE";
+	result = ft_strchr(str, 'T');
+	if (result)
+		printf("Encontrado: %s\n", result);
+	else
+		printf("Não encontrado\n");
+	return (0);
 }
