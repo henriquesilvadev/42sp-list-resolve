34d09bbdafbd508acc885da0290a065392fb5a59
Henrique Silva <contato@henriquesilva.dev>
2026-05-19 10:30:50 -0300

    feat(Libft) create ft_memchr.c

---
+++ Libft/ft_memchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhonorio <hhonorio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/19 09:01:41 by hhonorio          #+#    #+#             */
+/*   Updated: 2026/05/19 10:28:17 by hhonorio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*str;
+	size_t			i;
+	unsigned char	uc;
+
+	str = (unsigned char *) s;
+	uc = (unsigned char) c;
+	i = 0;
+	while (i < n)
+	{
+		if (str[i] == uc)
+			return ((void *) &str[i]);
+		i++;
+	}
+	return (NULL);
+}
