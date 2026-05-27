07844572e732a36464c29b2ede98347e1e407677
Henrique Honorio da Silva <contato@henriquesilva.dev>
2026-05-26 22:00:04 -0300

    feat: update ft_memmove.c

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -14,22 +14,25 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	*c_src;
-	char	*c_dst;
+	unsigned char	*c_src;
+	unsigned char	*c_dst;
 	size_t	i;
 
 	if (!dst && !src)
 		return (NULL);
-	c_src = (char *) src;
-	c_dst = (char *) dst;
+	c_src = (unsigned char *) src;
+	c_dst = (unsigned char *) dst;
 	i = 0;
 	if (c_dst > c_src)
 		while (len-- > 0)
 			c_dst[len] = c_src[len];
 	else
 	{
-		while (i++ < len)
+		while (i < len)
+		{
 			c_dst[i] = c_src[i];
+			i++;
+		}
 	}
 	return (dst);
 }
