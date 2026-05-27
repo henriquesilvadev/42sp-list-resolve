581f87a995eef2084e3515f1658b107f49d720ac
Henrique Silva <contato@henriquesilva.dev>
2026-05-27 10:20:31 -0300

    feat: update ft_split

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hhonorio <hhonorio@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: hhonorio <hhonorio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/24 17:17:10 by hhonorio          #+#    #+#             */
-/*   Updated: 2026/05/26 15:32:50 by hhonorio         ###   ########.fr       */
+/*   Updated: 2026/05/27 10:19:48 by hhonorio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 static int	word_counts(const char *str, char c);
-static char	**free_str(char **str, size_t j, int check);
+static char	**free_str(char **str, size_t j);
 
 char	**ft_split(char const *s, char c)
 {
@@ -22,26 +22,23 @@ char	**ft_split(char const *s, char c)
 	size_t	start;
 	char	**str;
 
-	if (!s)
-		return (NULL);
 	str = malloc(sizeof(char *) * (word_counts(s, c) + 1));
 	if (!str)
 		return (NULL);
 	i = 0;
 	j = 0;
-	start = 0;
 	while (s[i])
 	{
 		while (s[i] == c)
 			i++;
 		if (!s[i])
-			return (free_str(str, j, 1));
+			return (free_str(str, j));
 		start = i;
 		while (s[i] != c && s[i])
 			i++;
 		str[j++] = ft_substr(s, start, (i - start));
 		if (!str[j - 1])
-			return (free_str(str, j, 1));
+			return (free_str(str, j));
 	}
 	str[j] = NULL;
 	return (str);
@@ -63,13 +60,10 @@ static int	word_counts(const char *s, char c)
 	return (count);
 }
 
-static char	**free_str(char **str, size_t j, int check)
+static char	**free_str(char **str, size_t j)
 {
-	if (check)
-	{
-		while (j--)
-			free(str[j]);
-		free(str);
-	}
+	while (j--)
+		free(str[j]);
+	free(str);
 	return (NULL);
 }
