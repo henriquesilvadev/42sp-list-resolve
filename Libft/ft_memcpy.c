e25dadf9ee160991a85c41b12748cac6b6b08a22
Henrique Honorio da Silva <contato@henriquesilva.dev>
2026-05-26 21:51:53 -0300

    feat: update ft_memcpy.c

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -17,7 +17,7 @@ void	*ft_memcpy(void *dst,	const void *src, size_t n)
 	unsigned char	*tmp_dst;
 	unsigned char	*tmp_src;
 
-	if (dst == (void *)0 && src == (void *)0)
+	if (!dst || !src)
 		return (dst);
 	tmp_dst = (unsigned char *) dst;
 	tmp_src = (unsigned char *) src;
