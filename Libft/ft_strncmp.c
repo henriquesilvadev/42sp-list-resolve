c550f5d215c83001ed6fb6e6beb1399d204c4aef
Henrique Honorio da Silva <contato@henriquesilva.dev>
2026-05-26 22:56:30 -0300

    feat: update ft_strncmp.c

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -11,12 +11,13 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stddef.h>
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
+	if (n == 0)
+		return (0);
 	i = 0;
 	while (i < n)
 	{
