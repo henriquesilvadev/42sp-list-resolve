cca2995dea362a37fbdd3edc55bfa21325aa1efc
Henrique Silva <contato@henriquesilva.dev>
2026-05-27 08:51:57 -0300

    feat: update ft_lstadd_back.c

--- Libft/ft_lstadd_back.c
+++ Libft/ft_lstadd_back.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hhonorio <hhonorio@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: hhonorio <hhonorio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 08:47:41 by hhonorio          #+#    #+#             */
-/*   Updated: 2026/05/26 09:29:30 by hhonorio         ###   ########.fr       */
+/*   Updated: 2026/05/27 08:51:15 by hhonorio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
+
 	if (!lst || !new)
 		return ;
 	if (!*lst)
