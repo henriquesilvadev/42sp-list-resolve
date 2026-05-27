2d7a0584b6ae7ffe3ee825c5c814edf5eb4833db
Henrique Silva <contato@henriquesilva.dev>
2026-05-27 10:31:40 -0300

    feat: update ft_strlcat.c

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hhonorio <hhonorio@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: hhonorio <hhonorio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/24 15:02:44 by hhonorio          #+#    #+#             */
-/*   Updated: 2026/05/24 16:11:25 by hhonorio         ###   ########.fr       */
+/*   Updated: 2026/05/27 10:30:57 by hhonorio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	dst_len;
 	size_t	result;
 
-	if (!dst || !src)
-		return (0);
 	i = 0;
 	while (dst[i])
 		i++;
