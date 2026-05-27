fd39811b0494bdbcc530f845b702880660ae2f1b
Henrique Honorio da Silva <contato@henriquesilva.dev>
2026-05-26 21:14:49 -0300

    feat: update ft_lstmap.c

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -17,7 +17,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*result;
 	t_list	*tmp;
 
-	if (!lst)
+	if (!f || !del || !lst)
 		return (NULL);
 	result = NULL;
 	while (lst != NULL)
