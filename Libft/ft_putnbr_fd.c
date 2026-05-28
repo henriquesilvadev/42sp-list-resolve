d99e06f7409c87f1479a898d85408e53408882da
Henrique Silva <contato@henriquesilva.dev>
2026-05-28 09:37:05 -0300

    feat: update norma

--- Libft/ft_putnbr_fd.c
+++ Libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: hhonorio <hhonorio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 10:33:38 by hhonorio          #+#    #+#             */
-/*   Updated: 2026/05/28 09:10:46 by hhonorio         ###   ########.fr       */
+/*   Updated: 2026/05/28 09:32:50 by hhonorio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == INT_MIN)
 	{
-    	ft_putstr_fd("-2147483648", fd);
-    	return ;
+		ft_putstr_fd("-2147483648", fd);
+		return ;
 	}
 	if (n < 0)
 	{
@@ -26,9 +26,9 @@ void	ft_putnbr_fd(int n, int fd)
 	}
 	if (n < 10)
 	{
-		ft_putchar_fd(n + '0' , fd);	
+		ft_putchar_fd(n + '0', fd);
 		return ;
 	}
-	ft_putnbr_fd((n / 10) , fd);
-	ft_putchar_fd(n % 10 + '0' , fd);
+	ft_putnbr_fd((n / 10), fd);
+	ft_putchar_fd(n % 10 + '0', fd);
 }
