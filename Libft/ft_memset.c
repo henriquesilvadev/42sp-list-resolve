ca17a2dde952c37b593ad1bd962b9dff06a6d369
Henrique Silva <contato@henriquesilva.dev>
2026-05-31 09:15:10 -0300

    feat: update ft_memset

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: hhonorio <hhonorio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/17 09:39:31 by hhonorio          #+#    #+#             */
-/*   Updated: 2026/05/27 09:33:44 by hhonorio         ###   ########.fr       */
+/*   Updated: 2026/05/29 08:30:18 by hhonorio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ void	*ft_memset(void *b, int c, size_t len)
 	tmp = (unsigned char *) b;
 	while (len > 0)
 	{
-		*(tmp++) = (unsigned char) c;
+		*tmp = (unsigned char) c;
+		 tmp++;
 		len--;
 	}
 	return (b);
