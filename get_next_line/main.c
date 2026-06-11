ddf79ee340e29d0a8dcedf627f9f25993700ddde
Henrique Silva <contato@henriquesilva.dev>
2026-06-11 08:56:26 -0300

    feat: update main

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: hhonorio <hhonorio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/09 07:13:12 by hhonorio          #+#    #+#             */
-/*   Updated: 2026/06/09 07:14:13 by hhonorio         ###   ########.fr       */
+/*   Updated: 2026/06/11 08:37:01 by hhonorio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,27 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
 	int		fd;
 	char	*line;
 
-	fd = open("teste.txt", O_RDONLY);
+	if (argc != 0)
+	{
+		printf("linha: %s \n", argv[0]);
+	}
+	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
 	{
 		perror("open");
 		return (1);
 	}
-	while ((line = get_next_line(fd)) != NULL)
+	line = get_next_line(fd);
+	while (line != NULL)
 	{
 		printf("%s", line);
 		free(line);
+		line = get_next_line(fd);
 	}
 	close(fd);
 	return (0);
